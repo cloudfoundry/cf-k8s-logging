776df6d0b722b076aa10add4d14699df3ffd3250
Tom Chen <tochen@pivotal.io>
2020-01-21 11:02:52 -0700

    simplify deploy process
    - add scripts to generate and deploy certificates
    - add log-cache obects to repo from log-cache
    
    Signed-off-by: Ben Fuller <bfuller@pivotal.io>

--- scripts/init_certs.sh
+++ scripts/init_certs.sh
@@ -4,20 +4,18 @@ set -ex
 
 echo "Creating certs"
 
-certstrap init --common-name "LogsEgressCA" --passphrase "logsegress" --expires "2 years"
+certstrap init --common-name "LogsEgressCA" --passphrase "" --expires "2 years"
 
-certstrap request-cert --common-name "log-cache" --passphrase "logsegress"
-certstrap sign "log-cache" --CA "LogsEgressCA" --passphrase "logsegress"
+certstrap request-cert --common-name "log-cache" --passphrase ""
+certstrap sign "log-cache" --CA "LogsEgressCA" --passphrase ""
 
-certstrap request-cert --common-name "log-cache-metrics" --passphrase "logsegress"
-certstrap sign "log-cache-metrics" --CA "LogsEgressCA" --passphrase "logsegress"
+certstrap request-cert --common-name "log-cache-metrics" --passphrase ""
+certstrap sign "log-cache-metrics" --CA "LogsEgressCA" --passphrase ""
 
-certstrap request-cert --common-name "log-cache-gateway" --passphrase "logsegress"
-certstrap sign "log-cache-gateway" --CA "LogsEgressCA" --passphrase "logsegress"
+certstrap request-cert --common-name "log-cache-gateway" --passphrase ""
+certstrap sign "log-cache-gateway" --CA "LogsEgressCA" --passphrase ""
 
-certstrap request-cert --common-name "log-cache-syslog-server" --passphrase "logsegress"
-certstrap sign "log-cache-syslog-server" --CA "LogsEgressCA" --passphrase "logsegress"
+certstrap request-cert --common-name "log-cache-syslog" --passphrase ""
+certstrap sign "log-cache-syslog" --CA "LogsEgressCA" --passphrase ""
 
-echo "Creating K8s Secrets"
 
-kubectl 
