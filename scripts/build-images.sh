4faca61a90f3029489703b0b3d51b3ce5225436f
Jesse Weaver <jeweaver@pivotal.io>
2020-03-16 15:07:41 -0600

    Fix determination of image reference in build-images script

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -11,15 +11,15 @@ docker push $REPOSITORY/log-cache-cf-auth-proxy:latest
 docker push $REPOSITORY/syslog-server:latest
 docker push $REPOSITORY/cf-k8s-logging:latest
 
-syslog_digest=$(docker image inspect $REPOSITORY/syslog-server:latest --format {{.Id}})
-log_cache_digest=$(docker image inspect $REPOSITORY/log-cache:latest --format {{.Id}})
-log_cache_gateway_digest=$(docker image inspect $REPOSITORY/log-cache-gateway:latest --format {{.Id}})
-cf_auth_proxy_digest=$(docker image inspect $REPOSITORY/log-cache-cf-auth-proxy:latest --format {{.Id}})
-cf_k8s_logging_digest=$(docker image inspect $REPOSITORY/cf-k8s-logging:latest --format {{.Id}})
+syslog_reference="$(docker image inspect $REPOSITORY/syslog-server:latest --format '{{index .RepoDigests 0}}')"
+log_cache_reference="$(docker image inspect $REPOSITORY/log-cache:latest --format '{{index .RepoDigests 0}}')"
+log_cache_gateway_reference="$(docker image inspect $REPOSITORY/log-cache-gateway:latest --format '{{index .RepoDigests 0}}')"
+cf_auth_proxy_reference="$(docker image inspect $REPOSITORY/log-cache-cf-auth-proxy:latest --format '{{index .RepoDigests 0}}')"
+cf_k8s_logging_reference="$(docker image inspect $REPOSITORY/cf-k8s-logging:latest --format '{{index .RepoDigests 0}}')"
 
 #set the digests
-sed -i '' -e "s| syslog_server:.*| syslog_server: \"$REPOSITORY/syslog-server@$syslog_digest\"|" config/values.yml
-sed -i '' -e "s| log_cache:.*| log_cache: \"$REPOSITORY/log-cache@$log_cache_digest\"|" config/values.yml
-sed -i '' -e "s| log_cache_gateway:.*| log_cache_gateway: \"$REPOSITORY/log-cache-gateway@$log_cache_gateway_digest\"|" config/values.yml
-sed -i '' -e "s| cf_auth_proxy:.*| cf_auth_proxy: \"$REPOSITORY/log-cache-cf-auth-proxy@$cf_auth_proxy_digest\"|" config/values.yml
-sed -i '' -e "s| fluent:.*| fluent: \"$REPOSITORY/cf-k8s-logging@$cf_k8s_logging_digest\"|" config/values.yml
+sed -i '' -e "s| syslog_server:.*| syslog_server: \"$syslog_reference\"|" config/values.yml
+sed -i '' -e "s| log_cache:.*| log_cache: \"$log_cache_reference\"|" config/values.yml
+sed -i '' -e "s| log_cache_gateway:.*| log_cache_gateway: \"$log_cache_gateway_reference\"|" config/values.yml
+sed -i '' -e "s| cf_auth_proxy:.*| cf_auth_proxy: \"$cf_auth_proxy_reference\"|" config/values.yml
+sed -i '' -e "s| fluent:.*| fluent: \"$cf_k8s_logging_reference\"|" config/values.yml
