7913d4c68e453480174fda408dec04929bb4f875
Ben Fuller <bfuller@pivotal.io>
2020-05-20 15:53:42 +0000

    refactor build image script
    Signed-off-by: Travis Patterson <tpatterson@pivotal.io>

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,25 +1,19 @@
 REPOSITORY=${REPOSITORY:-logcache}
-docker build -t $REPOSITORY/log-cache:latest vendor/log-cache/src -f vendor/log-cache/src/cmd/log-cache/Dockerfile
-docker build -t $REPOSITORY/log-cache-gateway:latest vendor/log-cache/src -f vendor/log-cache/src/cmd/gateway/Dockerfile
-docker build -t $REPOSITORY/log-cache-cf-auth-proxy:latest vendor/log-cache/src -f vendor/log-cache/src/cmd/cf-auth-proxy/Dockerfile
-docker build -t $REPOSITORY/syslog-server:latest vendor/log-cache/src -f vendor/log-cache/src/cmd/syslog-server/Dockerfile
-docker build -t $REPOSITORY/cf-k8s-logging:latest vendor/cf-k8s-logging-fluent
 
-docker push $REPOSITORY/log-cache:latest
-docker push $REPOSITORY/log-cache-gateway:latest
-docker push $REPOSITORY/log-cache-cf-auth-proxy:latest
-docker push $REPOSITORY/syslog-server:latest
-docker push $REPOSITORY/cf-k8s-logging:latest
+function buildAndReplaceImage {
+    image=$1
+    srcFolder=$2
+    dockerfile=$3
+    yttValuesRef=$4
+    docker build -t $REPOSITORY/$image:latest $srcFolder -f $srcFolder/$dockerfile
+    docker push $REPOSITORY/$image:latest
 
-syslog_reference="$(docker image inspect $REPOSITORY/syslog-server:latest --format '{{index .RepoDigests 0}}')"
-log_cache_reference="$(docker image inspect $REPOSITORY/log-cache:latest --format '{{index .RepoDigests 0}}')"
-log_cache_gateway_reference="$(docker image inspect $REPOSITORY/log-cache-gateway:latest --format '{{index .RepoDigests 0}}')"
-cf_auth_proxy_reference="$(docker image inspect $REPOSITORY/log-cache-cf-auth-proxy:latest --format '{{index .RepoDigests 0}}')"
-cf_k8s_logging_reference="$(docker image inspect $REPOSITORY/cf-k8s-logging:latest --format '{{index .RepoDigests 0}}')"
+    imageRef="$(docker image inspect $REPOSITORY/$image:latest --format '{{index .RepoDigests 0}}')"
+    sed -i'' -e "s| $yttValuesRef:.*| $yttValuesRef: \"$imageRef\"|" config/values.yml
+}
 
-#set the digests
-sed -i'' -e "s| syslog_server:.*| syslog_server: \"$syslog_reference\"|" config/values.yml
-sed -i'' -e "s| log_cache:.*| log_cache: \"$log_cache_reference\"|" config/values.yml
-sed -i'' -e "s| log_cache_gateway:.*| log_cache_gateway: \"$log_cache_gateway_reference\"|" config/values.yml
-sed -i'' -e "s| cf_auth_proxy:.*| cf_auth_proxy: \"$cf_auth_proxy_reference\"|" config/values.yml
-sed -i'' -e "s| fluent:.*| fluent: \"$cf_k8s_logging_reference\"|" config/values.yml
+buildAndReplaceImage log-cache vendor/log-cache/src cmd/log-cache/Dockerfile log_cache
+buildAndReplaceImage log-cache-gateway vendor/log-cache/src cmd/log-cache-gateway/Dockerfile log_cache_gateway
+buildAndReplaceImage log-cache-cf-auth-proxy vendor/log-cache/src cmd/cf-auth-proxy/Dockerfile cf_auth_proxy
+buildAndReplaceImage syslog-server vendor/log-cache/src cmd/syslog-server/Dockerfile syslog_server
+buildAndReplaceImage cf-k8s-logging vendor/cf-k8s-logging-fluent Dockerfile fluent
