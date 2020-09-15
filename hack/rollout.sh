41417bbe43baaadf3d6a6e7fe51d2583d0cca9a5
Ben Fuller <bfuller@pivotal.io>
2020-09-15 20:34:05 +0000

    update rollout to look for cluster name at end of config(for gke)

--- hack/rollout.sh
+++ hack/rollout.sh
@@ -6,10 +6,10 @@ set -ex
 export CF_FOR_K8s_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s/}"
 export SCRIPT_DIR="$(cd $(dirname $0) && pwd -P)"
 export BASE_DIR="${SCRIPT_DIR}/.."
-
+export CF_NAME=${CF_NAME:-$(kubectl config current-context | rev | cut -d '_' -f -1 | rev)}
 $SCRIPT_DIR/../scripts/bump-cf-for-k8s.sh
 pushd "${CF_FOR_K8s_DIR}"
-  ytt -f config -f /tmp/cf.yml > /tmp/cf-rendered.yml
-  kapp deploy -a cf -f /tmp/cf-rendered.yml
+  ytt -f config -f /tmp/$CF_NAME.yml > /tmp/${CF_NAME}-rendered.yml
+  kapp deploy -a cf -f /tmp/${CF_NAME}-rendered.yml
 popd
 
