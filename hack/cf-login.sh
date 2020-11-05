ba32a5f6f6a0ce9bb04d7f07cf909595ad5be9d2
Louie Brann <lbrann@pivotal.io>
2020-11-05 00:25:58 +0000

    wip: trying to debug 503s

---
+++ hack/cf-login.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CF_CLI=${CF_CLI:-cf7} # or cf
+
+zone_name=${1:-sloans-lake}
+cf_domain="${zone_name}.loggr.cf-app.com"
+
+cf_admin_password="$(yq r /tmp/${zone_name}.yml cf_admin_password)"
+
+echo "Targeting cf on ${zone_name}"
+
+$CF_CLI api --skip-ssl-validation https://api.${cf_domain}
+$CF_CLI auth admin ${cf_admin_password}
+$CF_CLI create-org test-org
+$CF_CLI create-space -o test-org test-space
+$CF_CLI target -o test-org -s test-space
