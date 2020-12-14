ea7fcb564e9fbcc18aaadfdc62da1bcf72614822
Travis Patterson <tpatterson@pivotal.io>
2020-12-14 15:12:46 -0700

    remove set-pipline script in favor of /ci/configure

--- scripts/set-pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-fly -t denver set-pipeline -p cf-k8s-logging -c ./ci/cf-k8s-logging-validation.yml \
--l <(lpass show 'Shared-Loggregator (Pivotal Only)/pipeline-secrets.yml' --notes) \
--l <(lpass show 'Shared-CF- Log Cache (Pivotal ONLY)/release-credentials.yml' --notes) \
--l <(lpass show 'Shared Pivotal Common/pas-releng-fetch-releases' --notes) \
--l <(lpass show 'Shared-Loggregator (Pivotal Only)/cf-k8s-logging-pipeline-secrets.yml' --notes)
